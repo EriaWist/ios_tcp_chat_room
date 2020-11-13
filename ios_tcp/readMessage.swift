56bdd7cb3e514dd1b03f80ce66d34fb9396425e6
Eria <c121680118@gmail.com>
2020-11-14 00:03:08 +0800

    讓資料看起來像是往上推的方法

--- ios_tcp/readMessage.swift
+++ ios_tcp/readMessage.swift
@@ -4,6 +4,14 @@
 //
 //  Created by 阿騰 on 2020/11/13.
 //
+/*
+ 參考網址：https://www.coder.work/article/427566
+ 作法 把list倒轉180
+ 再把text顯示倒轉180
+ 最後將資料插入第0格
+ 這樣看起來就像下面往上推
+ 
+*/
 
 import SwiftUI
 import Network
@@ -17,12 +25,16 @@ struct readMessage: View {
                 ForEach(0..<messageData.count, id: \.self, content: {
                     (data) in
                     messageData[data]
+                        .rotationEffect(.radians(.pi))//再把text顯示倒轉180
+                        .scaleEffect(x: -1, y: 1, anchor: .center)
                 })
             }
+            .rotationEffect(.radians(.pi))//把list倒轉180
+            .scaleEffect(x: -1, y: 1, anchor: .center)
         }.onAppear(){
-            for i in 1...10 {
+            for i in 1...100 {
                 tcp.receive(minimumIncompleteLength: 1, maximumLength: 100) { (Data, ContentContext, Bool, NWError) in
-                    messageData.append(Text(String(data: Data!, encoding: .utf8)!))
+                    messageData.insert(Text(String(data: Data!, encoding: .utf8)!), at: 0)//最後將資料插入第0格
                 }
             }
             
